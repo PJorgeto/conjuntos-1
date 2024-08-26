dbb46bb89021be794c33879f2e77170266383f64
PJorgeto <166844906+PJorgeto@users.noreply.github.com>
2024-08-26 01:23:20 +0000

    Teste

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Ângelo Piovezan Jorgeto
+
+# o coódigo resolverá as operações de união, interseção, diferença e produto cartesiano, por exemplo o primeiro arquivo conjuntos.txt proposto pelo professor terá que resolver a união de conjunto 1: [3, 5, 67, 7] com conjunto 2: [1, 2, 3, 4] e a resposta deve ser [1, 2, 3, 4, 5, 67, 7]. A interseção de conjunto 1: [1, 2, 3, 4] com conjunto 2: [4, 5] deve ser [4, 5]. A diferença de conjunto 1: [1, A, C, 34] com conjunto 2: [A, C, D, 23] deve ser [1, 34, D, 23]. E o produto cartesiano de conjunto 1: [3, 4, 5, 5, A, B, R] com conjunto 2: [1, B, C, D, 1] a resposta deve ser: [('3', '1'), ('3', 'B'), ('3', 'C'), ('3', 'D'), ('3', '1'), ('4', '1'), ('4', 'B'), ('4', 'C'), ('4', 'D'), ('4', '1'), ('5', '1'), ('5', 'B'), ('5', 'C'), ('5', 'D'), ('5', '1'), ('5', '1'), ('5', 'B'), ('5', 'C'), ('5', 'D'), ('5', '1'), ('A', '1'), ('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', '1'), ('B', '1'), ('B', 'B'), ('B', 'C'), ('B', 'D'), ('B', '1'), ('R', '1'), ('R', 'B'), ('R', 'C'), ('R', 'D'), ('R', '1')]
 from collections import OrderedDict
 
 def realizar_operacoes(arquivo_entrada):
@@ -11,8 +14,8 @@ def realizar_operacoes(arquivo_entrada):
 
     num_operacoes = int(linhas[0].strip())
     index = 1  
-
-    resultados = []  # lista que armazena resultados
+    # lista que armazena resultados
+    resultados = []  
 
     for i in range(num_operacoes):
         operacao = linhas[index].strip()
